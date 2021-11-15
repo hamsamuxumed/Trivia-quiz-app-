badb460a15cb5122364640b4c47deeefe59bd901
Zain Uppal <zainuppal1@gmail.com>
2021-11-15 11:52:14 +0000

    add socket.io

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Socket} from './components/index'
+
+function App (){
+    return(
+        <div id='app'>
+            <Socket />
+        </div>
+    )
+}
+
+export default App;
