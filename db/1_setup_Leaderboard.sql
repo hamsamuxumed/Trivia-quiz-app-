d5df76dec80f6fd370f51ce9f296abae5d403319
hamsamuxumed <83721832+hamsamuxumed@users.noreply.github.com>
2021-11-17 16:26:24 +0000

    Delete db directory

--- db/1_setup_Leaderboard.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS Leaderboard;
-
-CREATE TABLE Leaderboard (
-    id SERIAL PRIMARY KEY,
-    username varchar(255) NOT NULL,
-    score int NOT NULL,
-    room varchar NOT NULL,
-    socket_id varchar(255) NOT NULL
-);
