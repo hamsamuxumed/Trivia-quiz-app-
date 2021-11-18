bea721f214e2cf8308f31f25ed393640c408d4e7
hamsamuxumed <83721832+hamsamuxumed@users.noreply.github.com>
2021-11-18 11:10:33 +0000

    add unit test for get all in models

--- api/tests/unit/models/Leaderboard.spec.js
+++ api/tests/unit/models/Leaderboard.spec.js
@@ -1,1 +1,27 @@
 const Leaderboard = require('../../../models/Leaderboard')
+
+const pg = require('pg');
+const { jest } = require('@jest/globals');
+jest.mock('pg')
+const db = require('../../../dbConfig/init');
+
+describe('Leaderboard', () => {
+    beforeEach(() => jest.clearAllMocks())
+
+    afterAll(() => jest.clearAllMocks())
+
+    //test for all leaderboards
+
+    describe('all', () => {
+        test('it resolves with all leaderboards on successful db query ', async () => {
+            jest.spyOn(db, 'query')
+                .mockResolvedValueOnce({ rows: [{}, {}, {}] });
+            const all = await Leaderboard.all;
+            expect(all).toHaveLength(3);
+        })
+    })
+
+
+
+
+})
